f0d53bbbed1211e599eb98a94ae7dfc3a0323b7f
Maheshkumar <callmeelprofessor@gmail.com>
2021-07-12 15:32:52 +0530

    Add temporary pop up

--- js/music.js
+++ js/music.js
@@ -2,6 +2,7 @@ const themeAudio = document.querySelector(".audio__theme");
 const hoverAudio = document.querySelector(".audio__hover");
 const clickAudio = document.querySelector(".audio__click");
 const options = document.querySelectorAll(".main__option");
+const popupButton = document.querySelector(".popup__button");
 
 function playMusic(){
     themeAudio.currentTime = 0;
@@ -24,4 +25,9 @@ options.forEach(option => {
     option.addEventListener("click", clickMusic);
 });
 
-playMusic();
+popupButton.addEventListener("click", (e) => {
+    document.body.classList.remove("blur");
+    document.querySelector(".popup").classList.add("none");
+    playMusic();
+})
+
--- js/util.js
+++ js/util.js
@@ -5,3 +5,4 @@ const infoCnt = document.querySelector(".header__icon:last-child");
 infoCnt.addEventListener("mouseenter", (e) => {
     //<span class="iconify" data-icon="fluent:book-question-mark-rtl-24-filled" data-inline="false"></span>
 });
+
