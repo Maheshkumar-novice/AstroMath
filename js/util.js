9f5e0d67d0aeb26907d0e28cb6b8aa540a6a1bf6
BLooDBRothER <computertechnology446@gmail.com>
2021-07-12 13:51:09 +0530

    Add hover effect

---
+++ js/util.js
@@ -0,0 +1,7 @@
+const infoIc = document.querySelector(".header__info-icon");
+const infoRtlIc = document.querySelector(".header__info-rtl-icon");
+const infoCnt = document.querySelector(".header__icon:last-child");
+
+infoCnt.addEventListener("mouseenter", (e) => {
+    //<span class="iconify" data-icon="fluent:book-question-mark-rtl-24-filled" data-inline="false"></span>
+});
