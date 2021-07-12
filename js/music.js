5b03d6a4494fefe30320908a5c58ff1c7e01c0b7
BLooDBRothER <computertechnology446@gmail.com>
2021-07-12 13:11:26 +0530

    Add music and hover click functionalitiy

---
+++ js/music.js
@@ -0,0 +1,27 @@
+const themeAudio = document.querySelector(".audio__theme");
+const hoverAudio = document.querySelector(".audio__hover");
+const clickAudio = document.querySelector(".audio__click");
+const options = document.querySelectorAll(".main__option");
+
+function playMusic(){
+    themeAudio.currentTime = 0;
+    themeAudio.play();
+    themeAudio.loop = "true";
+}
+
+function hoverMusic(){
+    hoverAudio.currentTime = 0;
+    hoverAudio.play();
+}
+
+function clickMusic(){
+    clickAudio.currentTime = 0;
+    clickAudio.play();
+}
+
+options.forEach(option => {
+    option.addEventListener("mouseenter", hoverMusic);
+    option.addEventListener("click", clickMusic);
+});
+
+playMusic();
