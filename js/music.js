0527abbe8f9479ad701aa2cdae2e9e785c1b97e9
Maheshkumar <callmeelprofessor@gmail.com>
2021-07-16 17:41:39 +0530

    Add something

--- js/music.js
+++ js/music.js
@@ -1,103 +1,77 @@
-const themeAudio = document.querySelector(".audio__theme");
-const hoverAudio = document.querySelector(".audio__hover");
-const clickAudio = document.querySelector(".audio__click");
-const options = document.querySelectorAll(".main__option");
-const popupButton = document.querySelector(".popup__button") || dummy;   
-const soundToggle = document.querySelector(".settings__sound--icon");
-let soundSrc, playable;
+import {
+  themeAudio,
+  hoverAudio,
+  clickAudio,
+  options,
+  popupButton,
+  soundToggle,
+  updateLocalSoundSrc,
+  getLocalSoundSrc,
+  handleThemePromise,
+  playMusic,
+  hoverMusic,
+  clickMusic,
+  checkPlayable,
+} from "./modules/music.js";
 
-function updateLocalSoundSrc(src){
-    localStorage.setItem("currentSoundSrc", src);
-}
+let soundSrc;
+let playable;
 
-function getLocalSoundSrc(){
-    return localStorage.getItem("currentSoundSrc");
-}
-
-function handleThemePromise(promise){
-    promise
-    .catch(err =>{
-        if(err){
-            soundToggle.src = "./assets/images/soundoff.svg";
-            updateLocalSoundSrc("./assets/images/soundoff.svg");
-        }
-    });
-}
-
-function playMusic(){
-    if(!playable)
-    {
-        themeAudio.pause();
-        return;
+options.forEach((option) => {
+  option.addEventListener("mouseenter", () => {
+    if (playable) {
+      hoverMusic();
     }
-    themeAudio.currentTime = 0;
-    let themePromise = themeAudio.play();
-    handleThemePromise(themePromise);
-    themeAudio.loop = "true";
-}
-
-function hoverMusic(){
-    if(!playable) return;
-    hoverAudio.currentTime = 0;
-    hoverAudio.play();
-}
-
-function clickMusic(){
-    if(!playable) return;
-    clickAudio.currentTime = 0;
-    clickAudio.play();
-}
-
-function checkPlayable(){
-    soundSrc = getLocalSoundSrc() || soundToggle.src;
-    return soundSrc.includes("soundon") ? true : false; 
-}
-
-options.forEach(option => {
-    option.addEventListener("mouseenter", hoverMusic);
-    option.addEventListener("click", clickMusic);
+  });
+  option.addEventListener("click", () => {
+    if (playable) {
+      clickMusic();
+    }
+  });
 });
 
 popupButton.addEventListener("click", (e) => {
-    document.body.classList.remove("blur");
-    document.body.classList.add("not-home");
-    popup.classList.remove("popup__active");
-    popup.classList.add("none");
-    if(checkPlayable()){
-        playMusic();
-    }
+  document.body.classList.remove("blur");
+  document.body.classList.add("not-home");
+  popup.classList.remove("popup__active");
+  popup.classList.add("none");
+  if (checkPlayable()) {
+    playMusic();
+  }
 });
 
 levelMenu.addEventListener("click", (e) => {
-    updateLocalSoundSrc(soundToggle.src);
+  updateLocalSoundSrc(soundToggle.src);
 });
 
 soundToggle.addEventListener("click", (e) => {
-    soundSrc = soundToggle.src;
-    if(soundSrc.includes("soundon")){
-        soundToggle.src = "./assets/images/soundoff.svg";
-        updateLocalSoundSrc("./assets/images/soundoff.svg");
-        playable = false;
-    }
-    else{
-        soundToggle.src = "./assets/images/soundon.svg";
-        updateLocalSoundSrc("./assets/images/soundon.svg");
-        playable = true;
-    }
-    console.log(popup.classList.contains("none"));
-    popup.classList.contains("popup__active") ? console.log("hello") : playMusic();
+  soundSrc = soundToggle.src;
+  if (soundSrc.includes("soundon")) {
+    soundToggle.src = "./assets/images/soundoff.svg";
+    updateLocalSoundSrc("./assets/images/soundoff.svg");
+    playable = false;
+  } else {
+    soundToggle.src = "./assets/images/soundon.svg";
+    updateLocalSoundSrc("./assets/images/soundon.svg");
+    playable = true;
+  }
+  console.log(popup.classList.contains("none"));
+  popup.classList.contains("popup__active")
+    ? console.log("hello")
+    : playMusic();
 });
 
-window.onload = function(){
-    if(document.body.classList.contains("not-home")){
-        soundToggle.src = getLocalSoundSrc() || soundToggle.src;
-        if(checkPlayable()){
-            playable = true;
-        }
-        playMusic();
+window.onload = function () {
+  if (document.body.classList.contains("not-home")) {
+    soundToggle.src = getLocalSoundSrc() || soundToggle.src;
+
+    if (checkPlayable()) {
+      playable = true;
     }
-    else{
-        getLocalSoundSrc() ? (soundToggle.src = getLocalSoundSrc(), playable = checkPlayable()) : (updateLocalSoundSrc(soundToggle.src), playable=true);
-        
-    }
+    playMusic();
+  } else {
+    getLocalSoundSrc()
+      ? ((soundToggle.src = getLocalSoundSrc()), (playable = checkPlayable()))
+      : (updateLocalSoundSrc(soundToggle.src), (playable = true));
+  }
 };
