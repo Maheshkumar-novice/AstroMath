afb7c62d4295a719ddd49d3a9d580618d20cc1ec
Maheshkumar <callmeelprofessor@gmail.com>
2021-07-20 20:37:35 +0530

    Add game_logic

--- js/game_logic.js
+++ js/game_logic.js
@@ -1,8 +1,52 @@
 import { getLocal, updateLocal } from "./modules/utils.js";
 
-let currentLevelData;
-window.addEventListener("load", () => {
-  currentLevelData = JSON.parse(getLocal("levelValue"));
-  currentLevelData[getLocal("gameLevel")][1] = 5;
-  updateLocal("levelValue", JSON.stringify(currentLevelData));
-});
+const operators = ["+", "-", "*", "/"];
+let qaMap = {};
+
+function getRandomOperator() {
+  return operators[Math.floor(Math.random() * operators.length)];
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10 + 1);
+}
+
+function returnAnswer(op1, op2, op) {
+  switch (op) {
+    case "+":
+      return op1 + op2;
+    case "-":
+      return op1 - op2;
+    case "*":
+      return op1 * op2;
+    case "/":
+      return +(op1 / op2).toFixed(2);
+    case "^":
+      return Math.pow(op1, op2);
+  }
+}
+
+function checkEquation(eq) {
+  return eq in qaMap;
+}
+
+function generateProblem() {
+  let [op1, op2, op] = [
+    getRandomNumber(),
+    getRandomNumber(),
+    getRandomOperator(),
+  ];
+  let eq = op1 + op + op2;
+  while (checkEquation(eq)) {
+    [op1, op2, op] = [
+      getRandomNumber(),
+      getRandomNumber(),
+      getRandomOperator(),
+    ];
+    eq = op1 + op + op2;
+  }
+  let ans = returnAnswer(op1, op2, op);
+  qaMap[eq] = [ans, ans + 10, ans - 10];
+}
+
+generateProblem();
